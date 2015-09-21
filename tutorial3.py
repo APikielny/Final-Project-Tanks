937f4caf3fb3b6e2b2e0fd757e18ab5e265a3343
APikielny <apikielny@gmail.com>
2015-09-21 14:32:37 -0400

    Updated from Brython Server: September 21, 2015 2:32:37 PM EDT

--- tutorial3.py
+++ tutorial3.py
@@ -1,2 +1,8 @@
 #Adam Pikielny
 #Graphics: Assets and Looping
+
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
+
+myapp = App()
+myapp.run()
