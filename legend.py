15106553ae6ed3b69d597c5e02a6b060b061f9c6
APikielny <apikielny@gmail.com>
2015-11-13 15:09:13 -0500

    Updated from Brython Server: November 13, 2015 3:09:15 PM EST

--- legend.py
+++ legend.py
@@ -20,10 +20,27 @@ class Tank(Sprite):
 
     def __init__(self, position):
         super().__init__(Tank.asset, position)
+        self.vx=1
+        self.vy=1
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        
+class Bullet(Sprite):
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(Tank.asset, position)
+        self.vx=5
+        self.vy=5
 
     def step(self):
         self.x += self.vx
         self.y += self.vy
+        #self.vy-=.5
         self.rotation += self.vr
 
 
@@ -36,9 +53,9 @@ class TankGame(App):
         bg = Sprite(bg_asset, (0,0))
         Tank((100,100))
 
-    """def step(self):
-        for ship in self.getSpritesbyClass(SpaceShip):
-            ship.step()"""
+    def step(self):
+        for ship in self.getSpritesbyClass(TankGame):
+            ship.step()
 
 
 myapp = TankGame(SCREEN_WIDTH, SCREEN_HEIGHT)
