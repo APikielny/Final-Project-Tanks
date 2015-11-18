0b063021f6efa488ea0fdf2a721516d03803ee3b
APikielny <apikielny@gmail.com>
2015-11-18 13:46:40 -0500

    Updated from Brython Server: November 18, 2015 1:46:42 PM EST

--- tank.py
+++ tank.py
@@ -5,12 +5,17 @@ class Tank(Sprite):
     asset = ImageAsset("images/tanks_by_fvsj-d6if9gt.png", Frame(100,550,350,160), 1, 'vertical')
     def __init__(self, position):
         super().__init__(Tank.asset, position)
-        self.vx=0
+        self.vx=1
         self.vy=0
         self.vr=0
         self.scale=.5
-
+        TankGame.listenKeyEvent("keydown", "left arrow", self.moveleft)
+    
     def step(self):
         self.x += self.vx
         self.y += self.vy
         self.rotation += self.vr
+
+    def moveleft(self,event):
+        self.vx+=5
+        
