d3de0a3923873ca2e5ff78d2adca169b7b11e684
APikielny <apikielny@gmail.com>
2015-11-18 13:32:44 -0500

    Update tank.py

--- tank.py
+++ tank.py
@@ -1,1 +1,16 @@
+class Tank(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/tanks_by_fvsj-d6if9gt.png", Frame(100,550,350,160), 1, 'vertical')
+    def __init__(self, position):
+        super().__init__(Tank.asset, position)
+        self.vx=0
+        self.vy=0
+        self.vr=0
+        self.scale=.5
 
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
