359e009a1bd8b178725b18c4355141edfb2a1a0f
APikielny <apikielny@gmail.com>
2015-11-18 13:46:50 -0500

    Updated from Brython Server: November 18, 2015 1:46:52 PM EST

--- tank.py
+++ tank.py
@@ -5,7 +5,7 @@ class Tank(Sprite):
     asset = ImageAsset("images/tanks_by_fvsj-d6if9gt.png", Frame(100,550,350,160), 1, 'vertical')
     def __init__(self, position):
         super().__init__(Tank.asset, position)
-        self.vx=1
+        self.vx=0
         self.vy=0
         self.vr=0
         self.scale=.5
