64f9a00ffb195f72428adf9eea8e830086a15523
APikielny <apikielny@gmail.com>
2015-11-18 13:39:15 -0500

    Update appclass.py

--- appclass.py
+++ appclass.py
@@ -11,28 +11,6 @@ import tank
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-
-        
-class Bullet(Sprite):
-    asset = ImageAsset("images/300px-BM_Grenade.png", Frame(100,20,100,115), 1, 'vertical')
-
-    def __init__(self, position):
-        super().__init__(Bullet.asset, position)
-        self.vx=1
-        self.vy=-.5
-        self.vr=1
-        self.scale=.5
-        self.vr=0
-        self.fxcenter = .5
-        self.fycenter = .5
-
-    def step(self):
-        self.x += self.vx
-        self.y += self.vy
-        self.vy += .05
-        self.rotation += .1
-
-
 class TankGame(App):
     def __init__(self, width, height):
         super().__init__(width, height)
