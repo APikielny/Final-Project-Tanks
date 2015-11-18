fa2742289d68820666e9de9a27847238b01647ff
APikielny <apikielny@gmail.com>
2015-11-18 13:32:40 -0500

    Update legend.py

--- legend.py
+++ legend.py
@@ -6,27 +6,12 @@ Tank Game
 """
 
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+import tank
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 
-class Tank(Sprite):
-    """
-    Animated space ship
-    """
-    asset = ImageAsset("images/tanks_by_fvsj-d6if9gt.png", Frame(100,550,350,160), 1, 'vertical')
-    def __init__(self, position):
-        super().__init__(Tank.asset, position)
-        self.vx=0
-        self.vy=0
-        self.vr=0
-        self.scale=.5
-
-    def step(self):
-        self.x += self.vx
-        self.y += self.vy
-        self.rotation += self.vr
         
 class Bullet(Sprite):
     asset = ImageAsset("images/300px-BM_Grenade.png", Frame(100,20,100,115), 1, 'vertical')
