a0dbdce5fae69c64e6069679fcae0a904263c2d8
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-12-07 14:37:36 -0500

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -21,7 +21,7 @@ class Bullet(Sprite):
     def step(self):
         self.x += self.vx
         self.y += self.vy
-        self.vy += .05
+        self.vy += .005
         self.rotation += .1
         if self.collidingWithSprites(Tank):  
             print("COLLIDING")
