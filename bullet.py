43776784bd15c2a1f9461bbffc8e8f2feb372fdf
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-12-09 13:42:12 -0500

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -24,11 +24,13 @@ class Bullet(Sprite):
         self.y += self.vy
         self.vy += .05
         self.rotation += .1
-        if self.collidingWithSprites(Tank):  
-            print("COLLIDING")
-            if self.away == True:
-                print("AWAY")
-                self.away = False
-        elif self.away == False:
-            print("NOT AWAY")
+        colliding = self.collidingWithSprites(Tank)
+        if colliding and self.away == True:  
             self.destroy()
+            #print("COLLIDING")
+            #if self.away == True:
+                #print("AWAY")
+                #self.away = False
+        elif not colliding and self.away == False:
+            #print("NOT AWAY")
+            self.away = True
