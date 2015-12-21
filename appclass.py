100c20790f9fc730a9199733c9894296a84d55a0
APikielny <apikielny@gmail.com>
2015-12-21 14:34:28 -0500

    Updated from Brython Server: December 21, 2015 2:34:29 PM EST

--- appclass.py
+++ appclass.py
@@ -17,7 +17,7 @@ class TankGame(App):
         super().__init__(width, height)
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        bg = RectangleAsset(700,100,noline,Color(0x00ff00,1.0))
+        bg = RectangleAsset(700,100,noline,Color(0x074a36,1.0))
         self.bg=Sprite(bg, (0,300))
         tankOne=Tank((200,75))
         bulletOne=Bullet((tankOne.position),self)
