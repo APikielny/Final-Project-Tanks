36b4f930069408ba71bb6dd67cfbde2ac1bc548f
APikielny <apikielny@gmail.com>
2015-12-22 14:17:34 -0500

    Update tank.py

--- tank.py
+++ tank.py
@@ -15,6 +15,8 @@ class Tank(Sprite):
         self.x += self.vx
         self.y += self.vy
         self.rotation += self.vr
+        if self.collidingWithSprites(Bullet):
+            self.destroy()
 
     """def moveleft(self,event):
         self.vx+=.5"""
