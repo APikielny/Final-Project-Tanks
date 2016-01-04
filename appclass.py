df432668745854a8aaddd8dec899f83e4429349a
APikielny <apikielny@gmail.com>
2016-01-04 14:47:26 -0500

    Updated from Brython Server: January 4, 2016 2:47:28 PM EST

--- appclass.py
+++ appclass.py
@@ -25,9 +25,9 @@ class TankGame(App):
         self.bg=Sprite(bg, (0,300))
         tankOne=Tank1((random.randint(20,150),265))
         tankTwo=Tank2((random.randint(350,500),265))
-        lol=input("This is the position of tank 1. Press return to continue with firing of Tank 1." + str(tankOne.position))
+        lol=input("This is the position of tank 1. Press return." + str(tankOne.position))
+        amirite=input("This is the position of tank 2. First, Player 1 will be prompted to fire from Tank 1. Next, Tank 2 will be prompted. Press return." + str(tankTwo.position))
         bulletOne=Bullet((tankOne.position),self)
-        amirite=input("This is the position of tank 2. Press return to continue with firing of Tank 2." + str(tankTwo.position))
         bulletTwo=Bullet((tankTwo.position),self)
         
         
