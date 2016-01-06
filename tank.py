14316f9f426ae2cdbd6c8cab8737fa5f1b25ec16
RDanilek <rogerdanilek@hanovernorwichschools.org>
2016-01-06 13:31:02 -0500

    Update tank.py

--- tank.py
+++ tank.py
@@ -7,7 +7,7 @@ class Tank1(Sprite):
         self.vx=0
         self.vy=0
         self.vr=0
-        self.scale=.25
+        self.scale=.05
         #TankGame.listenKeyEvent("keydown", "left arrow", self.moveleft)
         print("test")
         
